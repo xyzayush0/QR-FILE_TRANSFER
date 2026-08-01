510ad299b8e250e143fb4d0a447ce6bacd4a4ad4
Ayush Dey <ayushdey134@gmail.com>
2026-08-01 22:57:51 +0530

    Add TCP fallback for TURN server to bypass strict cellular UDP blocking

--- src/utils/webrtc.js
+++ src/utils/webrtc.js
@@ -5,8 +5,10 @@ export const initPeer = (customId, onOpen, onError) => {
     config: {
       'iceServers': [
         { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' }
+        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' }
       ]
     },
     debug: 2
